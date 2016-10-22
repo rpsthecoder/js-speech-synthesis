1feb6a90254fa6b9fbfa40dd92dbb8349787aab0
Preethi <rpsthecoder@users.noreply.github.com>
2016-10-22 14:30:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+if ('speechSynthesis' in window) with(speechSynthesis) {
+
+    document.querySelector('#play').addEventListener('click', onClickPlay);
+    document.querySelector('#pause').addEventListener('click', onClickPause);
+    
+    function onClickPlay() {
+        if(!speaking) { /* start narration */
+            utterance = new SpeechSynthesisUtterance(document.querySelector('article').textContent);
+            utterance.voice = getVoices()[0];
+            utterance.onend = ()=>{this.className = ''; };
+            this.className = 'played';
+            speak(utterance);
+        } else if (paused) { /* unpause/resume narration */
+            this.className = 'played';
+            this.nextElementSibling.className = '';
+            resume();
+        } 
+    }
+
+    function onClickPause() {
+        if(speaking && !paused){ /* pause narration */
+            this.className = 'paused';
+            this.previousElementSibling.className = '';
+            pause();
+        }
+    }
+
+}
+
+else { /* speech synthesis not supported */
+    msg = document.createElement('h5');
+    msg.textContent = "Detected no support for Speech Synthesis";
+    msg.style.textAlign = 'center';
+    msg.style.backgroundColor = 'red';
+    msg.style.color = 'white';
+    msg.style.marginTop = msg.style.marginBottom = 0;
+    document.body.insertBefore(msg, document.querySelector('div'));
+}
